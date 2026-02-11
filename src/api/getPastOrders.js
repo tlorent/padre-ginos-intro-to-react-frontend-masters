40a2577ed60de4c29beee4651300b90548ebfef1
Tim Lorent <tim.lorent@icloud.com>
2026-02-11 11:25:43 +0100

    fix: use the correct path for getpastorders

--- src/api/getPastOrders.js
+++ src/api/getPastOrders.js
@@ -1,7 +1,7 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export default async function getPastOrders(page) {
-  const response = await fetch(`${apiUrl}/api/past-order/${page}`);
+  const response = await fetch(`${apiUrl}/api/past-orders?page=${page}`);
   const data = await response.json();
   return data;
 }
